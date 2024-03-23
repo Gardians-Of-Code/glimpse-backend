b40eac27be7e4f1868df4ca11067f4ab336661e3
SarvasvaG <sarvasva_g@cs.iitr.ac.in>
2024-03-23 23:37:42 +0530

    Add Class with a constructor for initializing necessary attributes

--- bookmark.py
+++ bookmark.py
@@ -11,30 +11,33 @@ import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-
-nlp = spacy.load('en_core_web_sm')
 def spacy_tokenizer(doc):
-    return [t.lemma_ for t in nlp(doc) if not t.is_punct and not t.is_space and not t.is_stop and t.is_alpha]
-loaded_model=joblib.load("model.pkl")[1]
-text_extraction_tool = TextExtraction()
-text_classifier_tool = TextClassifier(loaded_model)
+        return [t.lemma_ for t in nlp(doc) if not t.is_punct and not t.is_space and not t.is_stop and t.is_alpha]
+    
+class Classifier:
+    def __init__(self):
+        global nlp
+        nlp = spacy.load('en_core_web_sm')
+        self.loaded_model=joblib.load("website_classifier_model.pkl")[1]
+        self.text_extraction_tool = TextExtraction()
+        self.text_classifier_tool = TextClassifier(self.loaded_model)
 
 
-async def get_tags_for_website(url):
-    name = text_extraction_tool.extract_name(url)
-    
-    tags=[]
-    text = await text_extraction_tool.extract_text([url])
-    
-    if text != ['Blocked']:
-        tags = text_classifier_tool.assign_tag(text, loaded_model)
-    
-    return name,tags
+    async def get_tags_for_website(self,url):
+        name = self.text_extraction_tool.extract_name(url)
+        
+        tags=[]
+        text = await self.text_extraction_tool.extract_text([url])
+        
+        if text != ['Blocked']:
+            tags = self.text_classifier_tool.assign_tag(text, self.loaded_model)
+        
+        return name,tags
     
         
 if __name__ == "__main__":
     
     url = input("Enter the url:\n")
-    name, tags = asyncio.run(get_tags_for_website(url))
+    name, tags = asyncio.run(Classifier().get_tags_for_website(url))
     print(f"Name: {name}\nTags: {tags}\n")
         
