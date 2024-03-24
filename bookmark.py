92fce96996a2adb7fd1c0f406abaaa8de0ac23b9
Souvik <souvikkarmakar8642@gmail.com>
2024-03-24 15:34:59 +0530

    ai backend working

--- bookmark.py
+++ bookmark.py
@@ -1,10 +1,6 @@
 from text_classify import TextClassifier
 from text_extraction import TextExtraction
 
-import asyncio
-import aiohttp
-
-# import spacy
 from tokenizer import *
 import joblib
 
@@ -12,33 +8,23 @@ import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-# def spacy_tokenizer(doc):
-#         return [t.lemma_ for t in nlp(doc) if not t.is_punct and not t.is_space and not t.is_stop and t.is_alpha]
-    
 class Classifier:
     def __init__(self):
         # global nlp
         # nlp = spacy.load('en_core_web_sm')
-        self.loaded_model=joblib.load("website_classifier_model.pkl")[1]
+        print("Loading model...")
+        self.loaded_model = joblib.load("website_classifier_model.pkl")[1]
         self.text_extraction_tool = TextExtraction()
         self.text_classifier_tool = TextClassifier(self.loaded_model)
 
-
-    async def get_tags_for_website(self,url):
+    async def get_tags_for_website(self, url):
         name = self.text_extraction_tool.extract_name(url)
-        
-        tags=[]
+
+        tags = []
         text = await self.text_extraction_tool.extract_text([url])
-        
-        if text != ['Blocked']:
+
+        if text != ["Blocked"]:
             tags = self.text_classifier_tool.assign_tag(text, self.loaded_model)
-        
-        return name,tags
-    
-        
-# if __name__ == "__main__":
-    
-#     url = input("Enter the url:\n")
-#     name, tags = asyncio.run(Classifier().get_tags_for_website(url))
-#     print(f"Name: {name}\nTags: {tags}\n")
-        
+
+        return name, tags
+
--- server.py
+++ server.py
@@ -1,24 +1,23 @@
-from flask import Flask, request, jsonify
-import asyncio
+from flask import Flask, request, jsonify, current_app
 from bookmark import *
 from tokenizer import *
 
 app = Flask(__name__)
 
-# classifier = Classifier()
+global classifier
+classifier = Classifier()
 
 
 @app.route("/api/v1/get_tags", methods=["POST"])
-async def get_tags():
+def get_tags():
     data = request.get_json()
     url = data["url"]
-    print(url)
-    name, tags = asyncio.run(classifier.get_tags_for_website(url))
-    print(name, tags)
+    # print(url)
+    name, tags = current_app.ensure_sync(classifier.get_tags_for_website)(url)
+    # print(name, tags)
     return jsonify({"name": name, "tags": tags})
 
 
 if __name__ == "__main__":
-    global classifier
-    classifier = Classifier()
+    
     app.run(debug=True, port=5000)
